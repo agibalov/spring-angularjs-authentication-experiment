8958c3c285b5458d9eae2cc38021a910b6dc49ca
loki2302 <loki2302@loki2302.me>
2014-04-01 23:43:33 +0400

    Initial version

---
+++ src/main/java/me/loki2302/App.java
@@ -0,0 +1,170 @@
+package me.loki2302;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfiguration.class, args);
+    }
+
+    @Configuration
+    @ComponentScan
+    @EnableAutoConfiguration
+    public static class AppConfiguration {
+
+    }
+
+    @Configuration
+    @EnableWebMvcSecurity
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    public static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.inMemoryAuthentication()
+                    .withUser("user").password("password").roles("USER");
+        }
+
+        @Bean
+        @Override
+        public AuthenticationManager authenticationManagerBean() throws Exception {
+            return super.authenticationManagerBean();
+        }
+
+        @Override
+        protected void configure(HttpSecurity httpSecurity) throws Exception {
+            httpSecurity.csrf().disable();
+        }
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            web.ignoring().antMatchers(
+                    "/",
+                    "/index.html",
+                    "/css/**",
+                    "/fonts/**",
+                    "/js/**");
+        }
+    }
+
+    @Configuration
+    public static class MvcConfiguration extends WebMvcConfigurerAdapter {
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        }
+    }
+
+    @RestController
+    @RequestMapping("/api")
+    public static class HomeController {
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+        public Me authenticate(@RequestBody AuthenticationRequest authenticationRequest) {
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                    authenticationRequest.username,
+                    authenticationRequest.password);
+
+            Authentication authentication;
+            try {
+                authentication = authenticationManager.authenticate(token);
+            } catch(AuthenticationException e) {
+                throw new InvalidCredentialsException();
+            }
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            Me me = new Me();
+            me.username = authentication.getName();
+            return me;
+        }
+
+        @PreAuthorize("isAuthenticated()")
+        @RequestMapping(value = "/me", method = RequestMethod.GET)
+        public Me getMe(Authentication authentication) {
+            Me me = new Me();
+            me.username = authentication.getName();
+            return me;
+        }
+
+        @PreAuthorize("isAuthenticated()")
+        @RequestMapping(value = "/deauthenticate", method = RequestMethod.POST)
+        public Me deauthenticate() {
+            SecurityContextHolder.clearContext();
+            return null;
+        }
+    }
+
+    @ControllerAdvice
+    public static class ErrorHandler {
+        @ExceptionHandler(InvalidCredentialsException.class)
+        @ResponseStatus(HttpStatus.UNAUTHORIZED)
+        @ResponseBody
+        public ErrorDto unauthorized(InvalidCredentialsException e) {
+            ErrorDto errorDto = new ErrorDto();
+            errorDto.message = "Unauthorized";
+            return errorDto;
+        }
+
+        @ExceptionHandler(AccessDeniedException.class)
+        @ResponseStatus(HttpStatus.FORBIDDEN)
+        @ResponseBody
+        public ErrorDto forbidden(AccessDeniedException e) {
+            ErrorDto errorDto = new ErrorDto();
+            errorDto.message = "Forbidden";
+            return errorDto;
+        }
+
+        @ExceptionHandler(RuntimeException.class)
+        @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+        @ResponseBody
+        public ErrorDto internalServerError(RuntimeException e) {
+            ErrorDto errorDto = new ErrorDto();
+            errorDto.message = "Runtime error";
+            return errorDto;
+        }
+    }
+
+    public static class Me {
+        public String username;
+    }
+
+    public static class AuthenticationRequest {
+        public String username;
+        public String password;
+    }
+
+    public static class ErrorDto {
+        public String message;
+    }
+
+    public static abstract class ApiException extends RuntimeException {
+    }
+
+    public static class InvalidCredentialsException extends ApiException {
+    }
+}
---
+++ src/test/java/me/loki2302/AppTest.java
@@ -0,0 +1,96 @@
+package me.loki2302;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+import static me.loki2302.App.*;
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AppConfiguration.class)
+@IntegrationTest
+@WebAppConfiguration
+public class AppTest {
+    private RestTemplate restTemplate;
+
+    @Before
+    public void setUpRestTemplate() {
+        ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        restTemplate = new RestTemplate(requestFactory);
+    }
+
+    @After
+    public void unsetRestTemplate() {
+        restTemplate = null;
+    }
+
+    @Test
+    public void notAuthenticatedByDefault() {
+        try {
+            restTemplate.getForObject("http://localhost:8080/api/me", Me.class);
+            fail("Managed to retrieve user details without authentication");
+        } catch(HttpClientErrorException e) {
+            assertEquals(HttpStatus.FORBIDDEN, e.getStatusCode());
+            System.out.printf("%s\n", e.getResponseBodyAsString());
+        }
+    }
+
+    @Test
+    public void cantAuthenticateWithInvalidCredentials() {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+        authenticationRequest.username = "hacker";
+        authenticationRequest.password = "123";
+
+        try {
+            restTemplate.postForObject(
+                    "http://localhost:8080/api/authenticate",
+                    authenticationRequest,
+                    Me.class);
+
+            fail("Managed to authenticate with invalid credentials");
+        } catch(HttpClientErrorException e) {
+            assertEquals(HttpStatus.UNAUTHORIZED, e.getStatusCode());
+        }
+
+        try {
+            restTemplate.getForObject("http://localhost:8080/api/me", Me.class);
+            fail("Managed to retrieve user details without authentication");
+        } catch(HttpClientErrorException e) {
+            assertEquals(HttpStatus.FORBIDDEN, e.getStatusCode());
+        }
+    }
+
+    @Test
+    public void canAuthenticateWithValidCredentials() {
+        AuthenticationRequest authenticationRequest = new AuthenticationRequest();
+        authenticationRequest.username = "user";
+        authenticationRequest.password = "password";
+
+        Me me = restTemplate.postForObject(
+                "http://localhost:8080/api/authenticate",
+                authenticationRequest,
+                Me.class);
+
+        assertNotNull(me);
+        assertEquals("user", me.username);
+
+        me = restTemplate.getForObject("http://localhost:8080/api/me", Me.class);
+        assertNotNull(me);
+        assertEquals("user", me.username);
+    }
+}
